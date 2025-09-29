19d09b860be5911c95befccfd36ee8deff86f39b
charanbantupalli <charanbantupalli321@gmail.com>
2025-09-29 21:16:55 +0530

    Task5 completed

---
+++ T5_BankAccountSimulation/Account.java
@@ -0,0 +1,56 @@
+package T5_BankAccountSimulation;
+
+
+
+public class Account {
+    private int accountNumber;
+    private String accountHolder;
+    private double balance;
+
+    // Constructor
+    public Account(int accountNumber, String accountHolder, double balance) {
+        this.accountNumber = accountNumber;
+        this.accountHolder = accountHolder;
+        this.balance = balance;
+    }
+
+    // Getters
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getAccountHolder() {
+        return accountHolder;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    // Deposit method
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposited: " + amount + " | New Balance: " + balance);
+        } else {
+            System.out.println("Invalid deposit amount.");
+        }
+    }
+
+    // Withdraw method
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrawn: " + amount + " | New Balance: " + balance);
+        } else {
+            System.out.println("Insufficient balance or invalid amount.");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Account [Account No: " + accountNumber +
+               ", Holder: " + accountHolder +
+               ", Balance: " + balance + "]";
+    }
+}
